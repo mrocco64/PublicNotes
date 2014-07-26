8b347aea3ba95f47209e61b3bf9f45c5a955c461
Mike Rocco <Mike@mikes-air.att.net>
2014-07-25 23:01:37 -0700

    untitled post stylizing

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,9 +1,3 @@
 class Note < ActiveRecord::Base
-	before_save :default_values
-
 	validates :note, presence: true
-
-	def default_values
-		self.title = self.title.present? ? self.title : "note #{Note.all.count + 1}"
-	end
 end
